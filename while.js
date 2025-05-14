013006e503064d297aa8957fc773b36346525301
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-14 12:52:55 -0300

    iterando o while

---
+++ while.js
@@ -0,0 +1,12 @@
+// A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste 
+// for avaliada como verdadeira. A condição é avaliada antes da execução da rotina.
+
+// O seguinte laço while itera enquanto n é menor que três.
+var n = 0; //2
+var x = 0; //3
+
+while (n < 3) { // 0,1,2
+  n++; // 3
+  x += n; // 6
+}
+console.log(x)
