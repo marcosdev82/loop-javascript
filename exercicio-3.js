69ea28e8eaa5e8e9e5c140bd90b5b55ce95ae07d
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-13 14:55:32 -0300

    exercicio 3

--- exercicio-3.js
+++ exercicio-3.js
@@ -8,6 +8,20 @@
 
 // 2. Identificar quais propriedades são numéricas (por exemplo, notas) e calcular a média dessas propriedades.
 
+// const aluno = {
+//   nome: "João Silva",
+//   idade: 20,
+//   curso: "Engenharia",
+//   nota1: 7.5,
+//   nota2: 8.0,
+//   nota3: 9.2,
+//   nota4: 6.8,
+// };
+
+// Você receberá um objeto que contém informações sobre um aluno, como nome, idade, curso e notas. Seu objetivo é:
+
+// 1. Iterar sobre as propriedades do objeto com `for...in`.
+
 const aluno = {
   nome: "João Silva",
   idade: 20,
@@ -17,17 +31,19 @@ const aluno = {
   nota3: 9.2,
   nota4: 6.8,
 };
-// 1. Iterar sobre as propriedades do objeto com `for...in`.
-for (let key in aluno) {
-  console.log(`${key}: ${aluno[key]}`)
-} 
+
+for(let key in aluno) {
+  console.log(`${key}:${aluno[key]}`)
+}
+
+console.log(`--------------------------`)
 
 // 2. Identificar quais propriedades são numéricas (por exemplo, notas) e calcular a média dessas propriedades.
-let notas = 0
-for (let key in aluno) {
 
+let media = 0
+for(let key in aluno) {
   if (typeof aluno[key] === 'number' && key !== 'idade') {
-    notas += aluno[key]
+    media+=aluno[key]
   }
 }
-console.log(notas / 4)
+console.log(`A média é: ${media/4}`)
